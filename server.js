24b8e7a50593646c07f2a884dd6873f9b296891e
sahibaahuja <sahibaahuja>
2019-05-05 22:39:37 -0400

    update

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+var PORT = process.env.PORT || 8080;
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+ 
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "survey.html"));
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
+
