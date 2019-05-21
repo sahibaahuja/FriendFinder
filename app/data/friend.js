ea0a6311646addddb6795bf36b7b2ae10bcb321b
sahibaahuja <sahibaahuja>
2019-05-21 15:13:06 -0400

    update

---
+++ app/data/friend.js
@@ -0,0 +1,28 @@
+var friends = [
+{
+    "name": "shelly",
+    "photo" : "",
+    "scores" : [ 5,1,4,4,5,1,2,5,4,1]
+
+}
+,
+
+{
+    "name": "sahib",
+    "photo" : "",
+    "scores" : [ 5,1,4,4,5,1,2,5,4,1]
+
+}
+
+,
+
+{
+    "name": "apaar",
+    "photo" : "",
+    "scores" : [ 5,1,4,4,5,1,2,5,4,1]
+
+}
+
+];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,27 +1,16 @@
 var express = require('express')
 var app = express()
-var bodyParser = require('body-parser')
+// var bodyParser = require('body-parser')
 var PORT = process.env.PORT || 8080;
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }))
  
 // parse application/json
-app.use(bodyParser.json())
+app.use(express.json())
  
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-})
-
-app.get("/survey", function(req, res) {
-  res.sendFile(path.join(__dirname, "survey.html"));
-});
+require("./app/routing/htmlRoutes")(app)
+// require("./app/routing/apiRoutes")(app)
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
